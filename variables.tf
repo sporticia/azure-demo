af6c4b74e6bdfc726ce6dea62feeb85e4aa9a8d6
Scott Brewerton <Scott.Brewerton@frasers.group>
2025-06-12 11:46:24 +0100

    feat/add subscription to workflow env vars

--- variables.tf
+++ variables.tf
@@ -7,3 +7,30 @@ variable "region_prefix" {
   type        = string
   description = "Short region string."
 }
+
+variable "vnet" {
+  type        = string
+  description = "vnet IP CIDR range"
+}
+
+variable "apps_subnet" {
+  type        = string
+  description = "apps_subnet IP CIDR range"
+}
+
+variable "private_ep_subnet" {
+  type        = string
+  description = "private_ep_subnet IP CIDR range"
+}
+
+variable "just_neu" {
+  type        = bool
+  default     = false
+  description = "For use in conditional resources that should only exist in North Europe"
+}
+
+variable "just_uks" {
+  type        = bool
+  default     = false
+  description = "For use in conditional resources that should only exist in UK South"
+}
